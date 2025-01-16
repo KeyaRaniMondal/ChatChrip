cf6949a1c8bcd9c2bf614a46187c5101aa3564bd
Sudip Kumar <keya24gadu2020@gmail.com>
2025-01-16 19:59:59 +0600

    image hosting functionality implemented

--- src/Dashboard/Post/AddPost.jsx
+++ src/Dashboard/Post/AddPost.jsx
@@ -1,50 +1,66 @@
+import { useState } from "react";
+import useAxiosPublic from "../../shared/useAxiosPublic";
+import useAxiosSecure from "../../shared/useAxiosSecure";
+import { useForm } from "react-hook-form";
 
-const AddPost=()=>{
+const image_hosting_key = import.meta.env.VITE_IMAGE_HOSTING_KEY
+const image_hosting_api = `https://api.imgbb.com/1/upload?key=${image_hosting_key}`
+const AddPost = () => {
+  const { register, handleSubmit } = useForm();
+  const axiosPublic = useAxiosPublic()
+  const axiosSecure = useAxiosSecure()
+  const [data, setData] = useState("");
+  const onSubmit = async (data) => {
+    console.log(data)
+    const imageFile = { image: data.image[0] }
+    const res = await axiosPublic.post(image_hosting_api, imageFile, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
+      }
+    })
+    if (res.data.success) {
+      const addPost = {
+        authorname: data.authorname,
+        postdescription: data.postdescription,
+        image: res.data.data.display_url
+      }
+      // const postRes=await axiosSecure.post('/addPost',addPost)
+      // console.log(postRes.data)
+      // if(menuRes.data.insertedId)
+      // {
 
-  
-    return (
-<div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+      // }
+    }
+    console.log(res.data)
+  }
+  return (
+    <div className="hero bg-base-200 min-h-screen">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form className="card-body" onSubmit={handleSubmit(onSubmit)}>
+            <label>Author Image</label>
+            <input {...register("image")}
+              type="file"
+              className="file-input file-input-bordered file-input-warning w-full max-w-xs" />
+            <label>Author Name</label>
+            <input {...register("authorname")} placeholder="Author name" />
+            <textarea {...register("postdescription")} placeholder="Post Description" />
+            {/* <label className="form-control w-full max-w-xs">
+              <div className="label">
+                <span className="label-text">Authore Image </span>
+              </div>
+              <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+            </label> */}
+            <p>{data}</p>
+            <input type="submit" />
+          </form>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Post Title</span>
-          </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
-        </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Post description</span>
-          </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
-        </div>
-      </form>
+      </div>
     </div>
-  </div>
-</div>
-    )
+  )
 }
 export default AddPost
+
+
+
 
